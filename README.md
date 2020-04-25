f62c0d4d97f42c5a95aa1d145b3ba95641007f57
Rohnak Agarwal <rrka79wal@gmail.com>
2020-04-25 21:05:29 +0530

    pretask

---
+++ README.md
@@ -0,0 +1,15 @@
+# INTERN TASKS
+
+## TASK-0
+Task-0 will make you understand how Buttons, EditTexts and TextViews interact with each other. So, what you have to do is build up an Android application containing the following widgets: -
+ - An EditText
+ - A Button
+ - A TextView
+Work of the widgets: -
+ - EditText – Takes a registration number as an input from the user.
+ - Button: - Tapping this button would change its color to green and would display the output in the TextView.
+ - TextView: - Would display the name of the person bearing the corresponding registration number.
+
+So, basically you will have to build an app that takes in registration number as input, then upon tapping the button, makes it green and displays the name of the person. 
+>Registration Number: 1801106398
+>Name: Rohnak Agarwal
---
+++ app/src/main/java/com/rssoft/intern_task0/MainActivity.java
@@ -0,0 +1,34 @@
+package com.rssoft.intern_task0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText regd = findViewById(R.id.reg_input);
+        Button btn = findViewById(R.id.btn);
+        final TextView name = findViewById(R.id.name_view);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (regd.getText().toString().isEmpty()) {
+                    name.setText("");
+                    return;
+                }
+                if (regd.getText().toString().equals("1801106398")) name.setText("Rohnak Agarwal");
+            }
+        });
+
+    }
+}
